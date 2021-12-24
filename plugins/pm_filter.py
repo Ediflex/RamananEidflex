c5d703793d1a142c5e821fe5bff1f48dd5151e55
Ediflex <94779490+Ediflex@users.noreply.github.com>
2021-12-24 11:48:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -435,9 +435,9 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('⚡️ 𝐌𝐚፝֟𝐬𝐭𝐞𝐫 ⚡️', url='https://t.me/Movie_World_00')
-            ],[
+            InlineKeyboardButton('🎞️ Search Movies', url='https://t.me/movie_bus6')
+            InlineKeyboardButton('⚡️ 𝐌𝐚፝֟𝐬𝐭𝐞𝐫 ⚡️', url='https://t.me/Mallu_mass')
+            InlineKeyboardButton('🔍 Search Inline', switch_inline_query_current_chat=''),
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
         ]]
@@ -466,7 +466,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         )
     elif query.data == "about":
         buttons= [[
-            InlineKeyboardButton('⚡️ 𝐌𝐚፝֟𝐬𝐭𝐞𝐫 ⚡️', url='https://t.me/Movie_World_00'),
+            InlineKeyboardButton('⚡️ 𝐌𝐚፝֟𝐬𝐭𝐞𝐫 ⚡️', url='https://t.me/Mallu_mass'),
             InlineKeyboardButton('♥️ Source', callback_data='source')
             ],[
             InlineKeyboardButton('🏠 Home', callback_data='start'),
